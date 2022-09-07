666f0105ea0df4091dcc0944584c10354d7e72fe
Adrian Bury <adrian.jakub.bury@gmail.com>
2022-09-07 23:43:29 +0200

    Add styles for login page; Create basic form

--- src/app/modules/auth/login/components/pages/login/login.page.ts
+++ src/app/modules/auth/login/components/pages/login/login.page.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,4 +7,27 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginPage {}
+export class LoginPage implements OnInit {
+  @ViewChild('loginInput', { static: true }) loginInput!: ElementRef<HTMLInputElement>;
+
+  loginForm = new FormGroup({
+    login: new FormControl('', {
+      validators: [Validators.required],
+      nonNullable: true,
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required],
+      nonNullable: true,
+    }),
+  });
+
+  ngOnInit(): void {
+    this.loginInput.nativeElement.focus();
+  }
+
+  sendForm() {
+    if (this.loginForm.valid) {
+      alert('Form sent!' + JSON.stringify(this.loginForm.value));
+    }
+  }
+}
--- src/app/modules/auth/login/login.module.ts
+++ src/app/modules/auth/login/login.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginPage } from './components/pages/login/login.page';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginPage],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     RouterModule.forChild([
       {
         path: '',
